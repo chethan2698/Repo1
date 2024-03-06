7aeec0201114d05e212de7eff8945b3fc7f353f8
chethan2698 <chnr403@gmail.com>
2024-03-06 13:49:21 +0530

    Code changes

--- Demo.java
+++ Demo.java
@@ -4,14 +4,10 @@ public class Demo {
 		
 		int x = 10;
 		int y = 20;
-		
-		
+		String message = "Welcome to git";		
 		System.out.println(x + y);
-		
-		
-		
+		System.out.println(message);
+				
 	}
-	
-	
-	
+
 }
